2a94aca9007cf7500e6c79d59ee40399471be9b1
Fabiel Rodrigues <fabiel.rodrigues@gmail.com>
2019-01-04 16:31:27 -0200

    Templates do site adaptados.

--- templates/site/contato.tpl.php
+++ templates/site/contato.tpl.php
@@ -1,3 +1,31 @@
 <h3>Preencha o formulário a seguir</h3>
 
-<a href="/">voltar</a>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae turpis velit. Nam suscipit ut lacus sed maximus. Nam gravida malesuada est, gravida volutpat mi aliquet quis. Nam hendrerit placerat ipsum vitae lobortis.</p>
+
+<div class="row">
+    <div class="col-1">
+        <h4>Fabiel Rodrigues</h4>
+
+        <p>R. Fulano de Tal, 999</p>
+        <p>CEP 71540-400</p>
+        <p>Distrito Federal - DF</p>
+        <P><a href="phone:619999988888">(61) 98888-6666</a></P>
+    </div>
+    <div class="col-2">
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="">Email</label>
+                <input name="from" type="email" class="form-control" placeholder="Seu email...">
+            </div>
+            <div class="form-group">
+                <label for="">Assunto</label>
+                <input name="subject" type="text" class="form-control" placeholder="Sua mensagem...">
+            </div>
+            <div class="form-group">
+                <label for="">Mensagem</label>
+                <textarea name="message" class="form-control" rows="5" placeholder="Sua mensagem"></textarea>
+            </div>
+            <input type="submit" value="Enviar" class="btn">
+        </form>
+    </div>
+</div>
