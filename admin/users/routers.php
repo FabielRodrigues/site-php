a05014ba4116edc138f523d57d76a925da30f58b
Fabiel Rodrigues <fabiel.rodrigues@gmail.com>
2019-01-04 10:22:33 -0200

    Finalizando os templates

--- admin/users/routers.php
+++ admin/users/routers.php
@@ -1,25 +1,30 @@
 <?php
+
 include __DIR__ . '/db.php';
 
-if (resolve('/admin/pages')) {
-    $pages = $pages_all();
-    render('admin/users/index', 'admin', ['users' => $users]);
+if (resolve('/admin/users')) {
+    $users = $users_all();
+    render('admin/users/index', 'admin', compact('users'));
+
 } elseif (resolve('/admin/users/create')) {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $users_create();
         return header('location: /admin/users');
     }
     render('admin/users/create', 'admin');
+
 } elseif ($params = resolve('/admin/users/(\d+)')) {
-    $page = $users_one($params[1]);
-    render('admin/users/view', 'admin', ['page' => $page]);
+    $user = $users_view($params[1]);
+    render('admin/users/view', 'admin', compact('user'));
+
 } elseif ($params = resolve('/admin/users/(\d+)/edit')) {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $users_edit($params[1]);
         return header('location: /admin/users/' . $params[1]);
     }
-    $page = $users_one($params[1]);
-    render('admin/users/edit', 'admin', ['page' => $page]);
+    $user = $users_view($params[1]);
+    render('admin/users/edit', 'admin', compact('user'));
+
 } elseif ($params = resolve('/admin/users/(\d+)/delete')) {
     $users_delete($params[1]);
     return header('location: /admin/users');
